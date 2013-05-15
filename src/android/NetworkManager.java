26f6c22049a3fb4f62470e0182ae03e03f93ca24
hermwong <herm.wong@gmail.com>
2013-05-15 16:04:29 -0700

    updated path to apache core package

--- src/android/NetworkManager.java
+++ src/android/NetworkManager.java
@@ -16,7 +16,7 @@
        specific language governing permissions and limitations
        under the License.
 */
-package org.apache.cordova;
+package org.apache.cordova.core;
 
 import org.apache.cordova.api.CallbackContext;
 import org.apache.cordova.api.CordovaInterface;
