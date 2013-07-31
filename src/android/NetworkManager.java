9b762951d4b4f396ff7f576326380d90b56d98a1
Andrew Grieve <agrieve@chromium.org>
2013-07-31 19:57:24 -0400

    [CB-4417] Move cordova-plugin-network-information to its own Java package.

--- src/android/NetworkManager.java
+++ src/android/NetworkManager.java
@@ -16,7 +16,7 @@
        specific language governing permissions and limitations
        under the License.
 */
-package org.apache.cordova.core;
+package org.apache.cordova.networkinformation;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaInterface;
